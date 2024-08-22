b8545e6bc09a8c89436d7fcd26e4ab7bad1b686d
Иван Лаврущев <ivan_olympiad@mail.ru>
2024-08-22 22:06:31 +0300

    array_of_tests and pointers

--- quadratic_equation.cpp
+++ quadratic_equation.cpp
@@ -2,11 +2,9 @@
 #include <math.h>
 #include <assert.h>
 
-#define MIN(a, b) ((a < b)? a : b)
-#define MAX(a, b) ((a > b)? a : b)
-
 const int NUMBER_OF_COEFFICIENT = 3;
-const double EPS = 0.001;
+const int NUMBER_OF_TESTS       = 8;
+const double EPS                = 0.001;
 
 enum NUMBER_OF_SOLUTIONS
 {
@@ -14,7 +12,7 @@ enum NUMBER_OF_SOLUTIONS
     ONE_SOLUTION          = 1,
     TWO_SOLUTIONS         = 2,
     INFINITY_OF_SOLUTIONS = -1,
-    INVALID = -2
+    INVALID               = -2
 };
 
 enum TYPES_OF_EQUATION
@@ -25,25 +23,19 @@ enum TYPES_OF_EQUATION
 
 enum DOUBLE_COMPARE
 {
-    LESS = -1,
+    LESS  = -1,
     EQUAL = 0,
-    MORE = 1
+    MORE  = 1
 };
 
-enum TEST_STATUS
-{
-    FALSE = 0,
-    TRUE = 1
-};
-
-struct answer
+struct ROOTS
 {
     NUMBER_OF_SOLUTIONS count_solutions;
     double x1;
     double x2;
 };
 
-struct testing_system
+struct TEST_PARAMETERS
 {
     int number_of_test;
     NUMBER_OF_SOLUTIONS count_solutions_exp;
@@ -52,28 +44,28 @@ struct testing_system
 };
 
 TYPES_OF_EQUATION find_type_of_square(double a, double b, double c);
-NUMBER_OF_SOLUTIONS solve_equation(double a, double b, double c, answer* ans);
-NUMBER_OF_SOLUTIONS solve_square(double a, double b, double c, answer* ans);
-NUMBER_OF_SOLUTIONS solve_linear(double b, double c, answer* ans);
+NUMBER_OF_SOLUTIONS solve_equation(double a, double b, double c, ROOTS* answer);
+NUMBER_OF_SOLUTIONS solve_square(double a, double b, double c, ROOTS* answer);
+NUMBER_OF_SOLUTIONS solve_linear(double b, double c, ROOTS* answer);
 void user_input(double* a, double* b, double* c);
-void result_output(NUMBER_OF_SOLUTIONS count_solutions, answer ans);
+void result_output(NUMBER_OF_SOLUTIONS count_solutions, ROOTS answer);
 int coefficient_check_finite(double a, double b, double c);
 DOUBLE_COMPARE double_comparing(double a, double b);
-void run_testing_system(void);
-TEST_STATUS run_test(testing_system test);
-TEST_STATUS compare_results(testing_system test, answer ans);
-TEST_STATUS test_or_user_input(void);
+void run_testing_system();
+bool run_test(TEST_PARAMETERS* test);
+bool compare_results(TEST_PARAMETERS* test, ROOTS answer);
+bool test_or_user_input();
 
 int main()
 {
     double a = NAN, b = NAN, c = NAN;
-    answer ans = {INVALID, NAN, NAN};
+    ROOTS answer = {INVALID, NAN, NAN};
 
     if (!test_or_user_input())
     {
         user_input(&a, &b, &c);
-        NUMBER_OF_SOLUTIONS count_solutions = solve_equation(a, b, c, &ans);
-        result_output(count_solutions, ans);
+        NUMBER_OF_SOLUTIONS count_solutions = solve_equation(a, b, c, &answer);
+        result_output(count_solutions, answer);
     }
     else
     {
@@ -87,7 +79,7 @@ void user_input(double* a, double* b, double* c)
 
     printf("Enter the number of the square equation: a b c\n");
     while ((input_count = scanf("%lg%lg%lg", a, b, c)) != NUMBER_OF_COEFFICIENT
-    && coefficient_check_finite(*a, *b, *c))
+    && coefficient_check_finite(*a, *b, *c)) // TODO while align
     {
         for (int i = 0; i < NUMBER_OF_COEFFICIENT - input_count; ++i)
         {
@@ -98,7 +90,7 @@ void user_input(double* a, double* b, double* c)
     }
 }
 
-void result_output(NUMBER_OF_SOLUTIONS count_solutions, answer ans)
+void result_output(NUMBER_OF_SOLUTIONS count_solutions, ROOTS answer)
 {
     switch (count_solutions)
     {
@@ -106,11 +98,12 @@ void result_output(NUMBER_OF_SOLUTIONS count_solutions, answer ans)
             printf("Equation has no solution\n");
             break;
         case ONE_SOLUTION:
-            printf("The equation has one solution: x1 = %.4lg\n", ans.x1);
+            printf("The equation has one solution: x1 = %.4lg\n", answer.x1);
             break;
         case TWO_SOLUTIONS:
-            printf("The equation has two solutions:\n"
-                    "x1 = %.4lg\tx2 = %.4lg\n", MIN(ans.x1, ans.x2), MAX(ans.x1, ans.x2));
+            printf("The equation has two solutions:\n x1 = %.4lg\tx2 = %.4lg\n",
+            ((double_comparing(answer.x1, answer.x2) == LESS)? answer.x1 : answer.x2),
+            ((double_comparing(answer.x1, answer.x2) == MORE)? answer.x1 : answer.x2));
             break;
         case INFINITY_OF_SOLUTIONS:
             printf("Equation has infinite number of solutions\n");
@@ -140,18 +133,18 @@ DOUBLE_COMPARE double_comparing(double a, double b){
     }
 }
 
-NUMBER_OF_SOLUTIONS solve_equation(double a, double b, double c, answer* ans)
+NUMBER_OF_SOLUTIONS solve_equation(double a, double b, double c, ROOTS* answer)
 {
-    assert(ans != NULL);
-    assert(&ans->x1 != NULL);
-    assert(&ans->x2 != NULL);
+    assert(answer != NULL);
+    assert(&answer->x1 != NULL);
+    assert(&answer->x2 != NULL);
 
     switch(find_type_of_square(a, b, c))
     {
         case LINEAR:
-            return solve_linear(b, c, ans);
+            return solve_linear(b, c, answer);
         case SQUARE:
-            return solve_square(a, b, c, ans);
+            return solve_square(a, b, c, answer);
         default:
             printf("ERROR: solve_equation");
 
@@ -171,10 +164,10 @@ TYPES_OF_EQUATION find_type_of_square(double a, double b, double c)
     }
 }
 
-NUMBER_OF_SOLUTIONS solve_linear(double b, double c, answer * ans)
+NUMBER_OF_SOLUTIONS solve_linear(double b, double c, ROOTS * answer)
 {
-    assert(ans != NULL);
-    assert(&ans->x1 != NULL);
+    assert(answer != NULL);
+    assert(&answer->x1 != NULL);
 
     if (double_comparing(b, 0) == EQUAL)
     {
@@ -184,20 +177,19 @@ NUMBER_OF_SOLUTIONS solve_linear(double b, double c, answer * ans)
     {
         if (double_comparing(c, 0) != EQUAL)
         {
-            ans->x1 = -c/b;
+            answer->x1 = -c/b;
         }
         else
         {
-            ans->x1 = 0;
+            answer->x1 = 0;
         }
         return ONE_SOLUTION;
     }
 }
 
-NUMBER_OF_SOLUTIONS solve_square(double a, double b, double c, answer * ans)
+NUMBER_OF_SOLUTIONS solve_square(double a, double b, double c, ROOTS * answer)
 {
-    assert(&ans->x1 != NULL);
-    assert(&ans->x2 != NULL);
+    assert(answer != NULL);
 
     double d = b*b - 4*a*c;
 
@@ -205,28 +197,28 @@ NUMBER_OF_SOLUTIONS solve_square(double a, double b, double c, answer * ans)
     {
         if (double_comparing(b, 0) != EQUAL)
         {
-            ans->x1 = -b/(2*a);
+            answer->x1 = -b/(2*a);
         }
         else
         {
-            ans->x1 = 0;
+            answer->x1 = 0;
         }
         return ONE_SOLUTION;
     }
     else if (double_comparing(d, 0) == MORE)
     {
         double sqrt_d = sqrt(d);
-        ans->x1 = (-b + sqrt_d)/(2*a);
-        ans->x2 = (-b - sqrt_d)/(2*a);
+        answer->x1 = (-b + sqrt_d)/(2*a);
+        answer->x2 = (-b - sqrt_d)/(2*a);
         if (double_comparing(c, 0) == EQUAL)
         {
-            if (double_comparing(ans->x1, 0) == EQUAL)
+            if (double_comparing(answer->x1, 0) == EQUAL)
             {
-                ans->x1 = 0;
+                answer->x1 = 0;
             }
             else
             {
-                ans->x2 = 0;
+                answer->x2 = 0;
             }
         }
         return TWO_SOLUTIONS;
@@ -237,157 +229,154 @@ NUMBER_OF_SOLUTIONS solve_square(double a, double b, double c, answer * ans)
     }
 }
 
-TEST_STATUS compare_results(testing_system test, answer ans)
+bool compare_results(TEST_PARAMETERS* test, ROOTS answer) // TODO const
 {
-    if (test.count_solutions_exp == ans.count_solutions)
+    if (test->count_solutions_exp == answer.count_solutions)
     {
-        switch(test.count_solutions_exp)
+        switch(test->count_solutions_exp)
         {
             case ZERO_SOLUTIONS:
-                if (isnan(ans.x1) && isnan(ans.x2))
+                if (isnan(answer.x1) && isnan(answer.x2))
                 {
-                    return TRUE;
+                    return true;
                 }
             case INFINITY_OF_SOLUTIONS:
-                if (isnan(ans.x1) && isnan(ans.x2))
+                if (isnan(answer.x1) && isnan(answer.x2))
                 {
-                    return TRUE;
+                    return true;
                 }
             case ONE_SOLUTION:
-                if ((double_comparing(test.x1_exp, ans.x1) == EQUAL) && isnan(ans.x2))
+                if ((double_comparing(test->x1_exp, answer.x1) == EQUAL) && isnan(answer.x2))
                 {
-                    return TRUE;
+                    return true;
                 }
             case TWO_SOLUTIONS:
-                if ((double_comparing(test.x1_exp, ans.x1) == EQUAL) &&
-                    (double_comparing(test.x2_exp, ans.x2) == EQUAL))
+                if ((double_comparing(test->x1_exp, answer.x1) == EQUAL) &&
+                    (double_comparing(test->x2_exp, answer.x2) == EQUAL))
                 {
-                    return TRUE;
+                    return true;
                 }
             case INVALID:
-                return FALSE;
+                return false;
             default:
-                return FALSE;
+                return false;
         }
     }
     else
     {
-        return FALSE;
+        return false;
     }
 }
 
-TEST_STATUS test_or_user_input(void)
+bool test_or_user_input()
 {
     printf("Run the test - Enter\n");
     printf("Run the user input something else\n");
     if ((getchar()) == '\n')
     {
-        return TRUE;
+        return true;
     }
     else
     {
-        while(getchar() != '\n')
-        ;
+        while(getchar() != '\n');
 
-        return FALSE;
+        return false;
     }
 }
 
-TEST_STATUS run_test(testing_system test)
+bool run_test(TEST_PARAMETERS* test) // TODO const
 {
-    TEST_STATUS status = TRUE;
-    answer ans = {INVALID, NAN, NAN};
+    ROOTS answer = {INVALID, NAN, NAN};
 
-    ans.count_solutions = solve_equation(test.a, test.b, test.c, &ans);
-    if (compare_results(test, ans))
+    answer.count_solutions = solve_equation(test->a, test->b, test->c, &answer);
+    if (compare_results(test, answer))
     {
-        printf("TEST №%d STATUS CORRECT\n", test.number_of_test);
+        printf("TEST №%d STATUS CORRECT\n", test->number_of_test);
 
-        return TRUE;
+        return true;
     }
     else
     {
-        printf("TEST №%d STATUS FALSE\n", test.number_of_test);
+        printf("TEST №%d STATUS false\n", test->number_of_test);
         printf("\tRESULT: count_solutions     = %d\tx1     = %.4lg\tx2     = %.4lg\n",
-                ans.count_solutions, ans.x1, ans.x2);
+                answer.count_solutions, answer.x1, answer.x2);
         printf("\tEXPECT: count_solutions_exp = %d\tx1_exp = %lg\tx2_exp = %lg\n",
-        test.count_solutions_exp, test.x1_exp, test.x2_exp);
+        test->count_solutions_exp, test->x1_exp, test->x2_exp);
 
-        return FALSE;
+        return false;
     }
 }
 
-void run_testing_system(void){
-    testing_system test1 = {.number_of_test      = 1,
-                            .a                   = 0,
-                            .b                   = 0,
-                            .c                   = 0,
-                            .count_solutions_exp = INFINITY_OF_SOLUTIONS,
-                            .x1_exp              = NAN,
-                            .x2_exp              = NAN};
+void run_testing_system(){
+    TEST_PARAMETERS array_of_tests[NUMBER_OF_TESTS] =
+    {
+        {.number_of_test     = 1,
+        .a                   = 0,
+        .b                   = 0,
+        .c                   = 0,
+        .count_solutions_exp = INFINITY_OF_SOLUTIONS,
+        .x1_exp              = NAN,
+        .x2_exp              = NAN},
 
-    testing_system test2 = {.number_of_test      = 2,
-                            .a                   = 0,
-                            .b                   = 0,
-                            .c                   = 1,
-                            .count_solutions_exp = ZERO_SOLUTIONS,
-                            .x1_exp              = NAN,
-                            .x2_exp              = NAN};
+        {.number_of_test     = 2,
+        .a                   = 0,
+        .b                   = 0,
+        .c                   = 1,
+        .count_solutions_exp = ZERO_SOLUTIONS,
+        .x1_exp              = NAN,
+        .x2_exp              = NAN},
 
-    testing_system test3 = {.number_of_test      = 3,
-                            .a                   = 0,
-                            .b                   = 1,
-                            .c                   = 0,
-                            .count_solutions_exp = ONE_SOLUTION,
-                            .x1_exp              = 0,
-                            .x2_exp              = NAN};
+        {.number_of_test     = 3,
+        .a                   = 0,
+        .b                   = 1,
+        .c                   = 0,
+        .count_solutions_exp = ONE_SOLUTION,
+        .x1_exp              = 0,
+        .x2_exp              = NAN},
 
-    testing_system test4 = {.number_of_test      = 4,
-                            .a                   = 1,
-                            .b                   = 0,
-                            .c                   = 0,
-                            .count_solutions_exp = ONE_SOLUTION,
-                            .x1_exp              = 0,
-                            .x2_exp              = NAN};
+        {.number_of_test     = 4,
+        .a                   = 1,
+        .b                   = 0,
+        .c                   = 0,
+        .count_solutions_exp = ONE_SOLUTION,
+        .x1_exp              = 0,
+        .x2_exp              = NAN},
 
-    testing_system test5 = {.number_of_test      = 5,
-                            .a                   = 1.5,
-                            .b                   = 1.5,
-                            .c                   = 1.5,
-                            .count_solutions_exp = ZERO_SOLUTIONS,
-                            .x1_exp              = NAN,
-                            .x2_exp              = NAN};
+        {.number_of_test     = 5,
+        .a                   = 1.5,
+        .b                   = 1.5,
+        .c                   = 1.5,
+        .count_solutions_exp = ZERO_SOLUTIONS,
+        .x1_exp              = NAN,
+        .x2_exp              = NAN},
 
-    testing_system test6 = {.number_of_test      = 6,
-                            .a                   = 0,
-                            .b                   = 4,
-                            .c                   = 3,
-                            .count_solutions_exp = ONE_SOLUTION,
-                            .x1_exp              = -0.75,
-                            .x2_exp              = NAN};
+        {.number_of_test     = 6,
+        .a                   = 0,
+        .b                   = 4,
+        .c                   = 3,
+        .count_solutions_exp = ONE_SOLUTION,
+        .x1_exp              = -0.75,
+        .x2_exp              = NAN},
 
-    testing_system test7 = {.number_of_test      = 7,
-                            .a                   = -3,
-                            .b                   = -5.7,
-                            .c                   = 0,
-                            .count_solutions_exp = TWO_SOLUTIONS,
-                            .x1_exp              = -1.9,
-                            .x2_exp              = 0};
+        {.number_of_test     = 7,
+        .a                   = -3,
+        .b                   = -5.7,
+        .c                   = 0,
+        .count_solutions_exp = TWO_SOLUTIONS,
+        .x1_exp              = -1.9,
+        .x2_exp              = 0},
 
-    testing_system test8 = {.number_of_test      = 8,
-                            .a                   = 1,
-                            .b                   = 5,
-                            .c                   = -4,
-                            .count_solutions_exp = TWO_SOLUTIONS,
-                            .x1_exp              = 0.7016,
-                            .x2_exp              = -5.702};
+        {.number_of_test     = 8,
+        .a                   = 1,
+        .b                   = 5,
+        .c                   = -4,
+        .count_solutions_exp = TWO_SOLUTIONS,
+        .x1_exp              = 0.7016,
+        .x2_exp              = -5.702}
+    };
 
-    run_test(test1);
-    run_test(test2);
-    run_test(test3);
-    run_test(test4);
-    run_test(test5);
-    run_test(test6);
-    run_test(test7);
-    run_test(test8);
+    for (int i = 0; i < NUMBER_OF_TESTS; ++i)
+    {
+        run_test(&array_of_tests[i]);
+    }
 }
