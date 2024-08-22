1350b64a683c6a0be226d2f68aebc9ab63a8051b
Иван Лаврущев <ivan_olympiad@mail.ru>
2024-08-22 11:33:22 +0300

    run_test function and struct.demo

--- quadratic_equation.cpp
+++ quadratic_equation.cpp
@@ -2,8 +2,6 @@
 #include <math.h>
 #include <assert.h>
 
- //TODO 0 0 0 error
-
 const int NUMBER_OF_COEFFICIENT = 3;
 const double EPS = 0.01;
 
@@ -29,11 +27,30 @@ enum DOUBLE_COMPARE
     MORE = 1
 };
 
+enum TEST_STATUS
+{
+    FALSE = 0,
+    CORRECT = 1
+};
+
 struct answer
 {
     NUMBER_OF_SOLUTIONS count_solutions;
     double x1;
     double x2;
+};
+
+struct testing_system
+{
+    NUMBER_OF_SOLUTIONS count_solutions_exp;
+    struct coefficient
+    {
+        double a, b, c;
+    };
+    struct roots
+    {
+        double x1_exp, x2_exp;
+    };
 };
 
 TYPES_OF_EQUATION find_type_of_square(double a, double b, double c);
@@ -45,16 +62,20 @@ void result_output(NUMBER_OF_SOLUTIONS count_solutions, answer ans);
 int coefficient_check_finite(double a, double b, double c);
 DOUBLE_COMPARE double_comparing(double a, double b);
 //int run_testing_system(void);
-//int run_test(int count_test, a, b, c, );
+TEST_STATUS run_test(int number_of_test, double a, double b, double c, NUMBER_OF_SOLUTIONS count_solutions_exp, double x1_exp, double x2_exp);
+TEST_STATUS compare_results(NUMBER_OF_SOLUTIONS count_solutions_exp, double x1_exp, double x2_exp,
+                    NUMBER_OF_SOLUTIONS count_solutions, double x1, double x2);
 
 int main()
 {
     double a = NAN, b = NAN, c = NAN;
     answer ans = {INVALID, NAN, NAN};
 
-    user_input(&a, &b, &c);
-    NUMBER_OF_SOLUTIONS count_solutions = solve_equation(a, b, c, &ans);
-    result_output(count_solutions, ans);
+    // user_input(&a, &b, &c);
+    // NUMBER_OF_SOLUTIONS count_solutions = solve_equation(a, b, c, &ans);
+    // result_output(count_solutions, ans);
+    run_test(1, 0, 0, 0, INFINITY_OF_SOLUTIONS, NAN, NAN);
+
 }
 
 void user_input(double* a, double* b, double* c)
@@ -151,11 +172,10 @@ NUMBER_OF_SOLUTIONS solve_linear(double b, double c, answer * ans)
 {
     assert(ans != NULL); //BUG when link could be NULL and what link has NAN
     assert(&ans->x1 != NULL);
-    assert(!isnan(ans->x1));
 
-    if (double_comparing(b, 0) == LESS or double_comparing(b, 0) == EQUAL)
+    if (double_comparing(b, 0) == EQUAL)
     {
-        return (double_comparing(c, 0) == LESS or double_comparing(c, 0) == EQUAL) ? INFINITY_OF_SOLUTIONS : ZERO_SOLUTIONS;
+        return (double_comparing(c, 0) == EQUAL) ? INFINITY_OF_SOLUTIONS : ZERO_SOLUTIONS;
     }
     else
     {
@@ -172,14 +192,13 @@ NUMBER_OF_SOLUTIONS solve_square(double a, double b, double c, answer * ans)
 
     double d = b*b - 4*a*c;
 
-    // TODO write compareDoubles()
-    if (fabs(d) < EPS)
+    if (double_comparing(d, 0) == EQUAL)
     {
         ans->x1 = -b/(2*a);
 
         return ONE_SOLUTION;
     }
-    else if (d > EPS)
+    else if (double_comparing(d, 0) == MORE)
     {
         double sqrt_d = sqrt(d);
         ans->x1 = (-b + sqrt_d)/(2*a);
@@ -190,5 +209,42 @@ NUMBER_OF_SOLUTIONS solve_square(double a, double b, double c, answer * ans)
     else
     {
         return ZERO_SOLUTIONS;
+    }
+}
+
+TEST_STATUS compare_results(NUMBER_OF_SOLUTIONS count_solutions_exp, double x1_exp, double x2_exp,
+                    NUMBER_OF_SOLUTIONS count_solutions, double x1, double x2)
+{
+    if ((count_solutions_exp == count_solutions) && (x1_exp == x1) && (x2_exp == x2))
+    {
+        return CORRECT;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+TEST_STATUS run_test(int number_of_test, double a, double b, double c,
+            NUMBER_OF_SOLUTIONS count_solutions_exp, double x1_exp, double x2_exp)
+{
+    TEST_STATUS status = CORRECT;
+    double x1 = NAN, x2 = NAN;
+    NUMBER_OF_SOLUTIONS count_colutions = INVALID;
+
+    count_colutions = solve_equation(a, b, c, &x1, &x2);
+    if (compare_results(count_solutions_exp, x1_exp, x2_exp, count_colutions, x1, x2))
+    {
+        printf("TEST №%d STATUS CORRECT\n", number_of_test);
+
+        return CORRECT;
+    }
+    else
+    {
+        printf("TEST STATUS FLASE\n");
+        printf("RESULT: count_solutions = %d\tx1 = %lg\tx2 = %lg\n", count_colutions, x1, x2);
+        printf("EXPECT: count_solutions_exp = %d\tx1_exp = %lg\tx2_exp = %lg\t", count_solutions_exp, x1_exp, x2_exp);
+
+        return FALSE;
     }
 }
