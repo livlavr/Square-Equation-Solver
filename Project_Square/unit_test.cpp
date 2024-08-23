a146bee14949d03e1a069e87c2ab34b90e63d4f5
Иван Лаврущев <ivan_olympiad@mail.ru>
2024-08-23 16:46:38 +0300

    ded32 flags fix

--- Project_Square/unit_test.cpp
+++ Project_Square/unit_test.cpp
@@ -15,28 +15,22 @@ bool compare_results(const TEST_PARAMETERS* test, ROOTS answer)
         switch(test->count_solutions_exp)
         {
             case ZERO_SOLUTIONS:
-                if (isnan(answer.x1) && isnan(answer.x2))
-                {
-                    return true;
-                }
+                return (isnan(answer.x1) && isnan(answer.x2));
+                break;
             case INFINITY_OF_SOLUTIONS:
-                if (isnan(answer.x1) && isnan(answer.x2))
-                {
-                    return true;
-                }
+                return (isnan(answer.x1) && isnan(answer.x2));
+                break;
             case ONE_SOLUTION:
-                if ((double_comparing(test->x1_exp, answer.x1) == EQUAL) && isnan(answer.x2))
-                {
-                    return true;
-                }
+                return ((double_comparing(test->x1_exp, answer.x1) == EQUAL)
+                        && isnan(answer.x2));
+                break;
             case TWO_SOLUTIONS:
-                if ((double_comparing(test->x1_exp, answer.x1) == EQUAL) &&
-                    (double_comparing(test->x2_exp, answer.x2) == EQUAL))
-                {
-                    return true;
-                }
+                return ((double_comparing(test->x1_exp, answer.x1) == EQUAL) &&
+                    (double_comparing(test->x2_exp, answer.x2) == EQUAL));
+                break;
             case INVALID:
                 return false;
+                break;
             default:
                 return false;
         }
@@ -47,14 +41,14 @@ bool compare_results(const TEST_PARAMETERS* test, ROOTS answer)
     }
 }
 
-bool run_test(TEST_PARAMETERS* const test)
+bool run_test(const TEST_PARAMETERS* test)
 {
     ROOTS answer = {INVALID, NAN, NAN};
 
     answer.count_solutions = solve_equation(test->a, test->b, test->c, &answer);
     if (compare_results(test, answer))
     {
-        printf("TEST №%d STATUS CORRECT\n", test->number_of_test);
+        printf("TEST №%d STATUS CORRECT\n\n", test->number_of_test);
 
         return true;
     }
@@ -63,7 +57,7 @@ bool run_test(TEST_PARAMETERS* const test)
         printf("TEST №%d STATUS false\n", test->number_of_test);
         printf("\tRESULT: count_solutions     = %d\tx1     = %.4lg\tx2     = %.4lg\n",
                 answer.count_solutions, answer.x1, answer.x2);
-        printf("\tEXPECT: count_solutions_exp = %d\tx1_exp = %lg\tx2_exp = %lg\n",
+        printf("\tEXPECT: count_solutions_exp = %d\tx1_exp = %lg\tx2_exp = %lg\n\n",
         test->count_solutions_exp, test->x1_exp, test->x2_exp);
 
         return false;
@@ -73,74 +67,74 @@ bool run_test(TEST_PARAMETERS* const test)
 void run_testing_system(){
     TEST_PARAMETERS array_of_tests[NUMBER_OF_TESTS] =
     {
-        {.number_of_test     = 1,
+       {.number_of_test      = 0,
+        .count_solutions_exp = INFINITY_OF_SOLUTIONS,
         .a                   = 0,
         .b                   = 0,
         .c                   = 0,
-        .count_solutions_exp = INFINITY_OF_SOLUTIONS,
         .x1_exp              = NAN,
         .x2_exp              = NAN},
 
-        {.number_of_test     = 2,
+       {.number_of_test      = 1,
+        .count_solutions_exp = ZERO_SOLUTIONS,
         .a                   = 0,
         .b                   = 0,
         .c                   = 1,
-        .count_solutions_exp = ZERO_SOLUTIONS,
         .x1_exp              = NAN,
         .x2_exp              = NAN},
 
-        {.number_of_test     = 3,
+       {.number_of_test      = 2,
+        .count_solutions_exp = ONE_SOLUTION,
         .a                   = 0,
         .b                   = 1,
         .c                   = 0,
-        .count_solutions_exp = ONE_SOLUTION,
         .x1_exp              = 0,
         .x2_exp              = NAN},
 
-        {.number_of_test     = 4,
+       {.number_of_test      = 3,
+        .count_solutions_exp = ONE_SOLUTION,
         .a                   = 1,
         .b                   = 0,
         .c                   = 0,
-        .count_solutions_exp = ONE_SOLUTION,
         .x1_exp              = 0,
         .x2_exp              = NAN},
 
-        {.number_of_test     = 5,
+       {.number_of_test      = 4,
+        .count_solutions_exp = ZERO_SOLUTIONS,
         .a                   = 1.5,
         .b                   = 1.5,
         .c                   = 1.5,
-        .count_solutions_exp = ZERO_SOLUTIONS,
         .x1_exp              = NAN,
         .x2_exp              = NAN},
 
-        {.number_of_test     = 6,
-        .a                   = 0,
-        .b                   = 4,
-        .c                   = 3,
-        .count_solutions_exp = ONE_SOLUTION,
+       {.number_of_test      =  5,
+        .count_solutions_exp =  ONE_SOLUTION,
+        .a                   =  0,
+        .b                   =  4,
+        .c                   =  3,
         .x1_exp              = -0.75,
-        .x2_exp              = NAN},
+        .x2_exp              =  NAN},
 
-        {.number_of_test     = 7,
+       {.number_of_test      =  6,
+        .count_solutions_exp =  TWO_SOLUTIONS,
         .a                   = -3,
         .b                   = -5.7,
-        .c                   = 0,
-        .count_solutions_exp = TWO_SOLUTIONS,
+        .c                   =  0,
         .x1_exp              = -1.9,
-        .x2_exp              = 0},
+        .x2_exp              =  0},
 
-        {.number_of_test     = 8,
-        .a                   = 1,
-        .b                   = 5,
+       {.number_of_test      =  7,
+        .count_solutions_exp =  TWO_SOLUTIONS,
+        .a                   =  1,
+        .b                   =  5,
         .c                   = -4,
-        .count_solutions_exp = TWO_SOLUTIONS,
-        .x1_exp              = 0.7016,
+        .x1_exp              =  0.7016,
         .x2_exp              = -5.702}
     };
 
     for (int i = 0; i < NUMBER_OF_TESTS; ++i)
     {
-        run_test(&array_of_tests[i]); // TODO color output
-                                      // TODO message when test is starting
+        printf("TEST №%d STATUS running...\n", i);
+        run_test(&array_of_tests[i]);
     }
 }
--- Project_Square/unit_test.h
+++ Project_Square/unit_test.h
@@ -3,14 +3,14 @@
 
 struct TEST_PARAMETERS
 {
-    int number_of_test = -1;
+    int number_of_test = 0;
     NUMBER_OF_SOLUTIONS count_solutions_exp = INVALID;
-    double a = NAN, b = NAN, c = NAN;
-    double x1_exp = NAN, x2_exp = NAN;
+    double a = 0, b = 0, c = 0;
+    double x1_exp = 0, x2_exp = 0;
 };
 
 void run_testing_system();
-bool run_test(TEST_PARAMETERS* test);
+bool run_test(const TEST_PARAMETERS* test);
 bool compare_results(const TEST_PARAMETERS* test, ROOTS answer);
 
 #endif
